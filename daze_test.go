42b246c31a6b79cac2ff329edb9cde7756a4f320
mohanson <mohanson@outlook.com>
2026-01-23 22:13:33 +0800

    Remove duplicate test cases

--- daze_test.go
+++ daze_test.go
@@ -15,16 +15,14 @@ const (
 	HostLookup         = "google.com"
 )
 
-func TestLocaleHTTP(t *testing.T) {
+func TestLocaleHttp(t *testing.T) {
 	locale := NewLocale(DazeServerListenOn, &Direct{})
 	defer locale.Close()
 	locale.Run()
 
 	cmd := exec.Command("curl", "-x", "http://"+DazeServerListenOn, CurlDest)
 	out := doa.Try(cmd.Output())
-	if !bytes.Contains(out, []byte("zhihu")) {
-		t.FailNow()
-	}
+	doa.Doa(bytes.Contains(out, []byte("zhihu")))
 }
 
 func TestLocaleSocks4(t *testing.T) {
@@ -34,9 +32,7 @@ func TestLocaleSocks4(t *testing.T) {
 
 	cmd := exec.Command("curl", "-x", "socks4://"+DazeServerListenOn, CurlDest)
 	out := doa.Try(cmd.Output())
-	if !bytes.Contains(out, []byte("zhihu")) {
-		t.FailNow()
-	}
+	doa.Doa(bytes.Contains(out, []byte("zhihu")))
 }
 
 func TestLocaleSocks4a(t *testing.T) {
@@ -46,9 +42,7 @@ func TestLocaleSocks4a(t *testing.T) {
 
 	cmd := exec.Command("curl", "-x", "socks4a://"+DazeServerListenOn, CurlDest)
 	out := doa.Try(cmd.Output())
-	if !bytes.Contains(out, []byte("zhihu")) {
-		t.FailNow()
-	}
+	doa.Doa(bytes.Contains(out, []byte("zhihu")))
 }
 
 func TestLocaleSocks5(t *testing.T) {
@@ -58,33 +52,7 @@ func TestLocaleSocks5(t *testing.T) {
 
 	cmd := exec.Command("curl", "-x", "socks5://"+DazeServerListenOn, CurlDest)
 	out := doa.Try(cmd.Output())
-	if !bytes.Contains(out, []byte("zhihu")) {
-		t.FailNow()
-	}
-}
-
-func TestResolverDns(t *testing.T) {
-	dns := ResolverDns(ResolverPublic.Cloudflare.Dns)
-	_, err := dns.LookupHost(context.Background(), HostLookup)
-	if err != nil {
-		t.FailNow()
-	}
-}
-
-func TestResolverDot(t *testing.T) {
-	dot := ResolverDot(ResolverPublic.Cloudflare.Dot)
-	_, err := dot.LookupHost(context.Background(), HostLookup)
-	if err != nil {
-		t.FailNow()
-	}
-}
-
-func TestResolverDoh(t *testing.T) {
-	doh := ResolverDoh(ResolverPublic.Cloudflare.Doh)
-	_, err := doh.LookupHost(context.Background(), HostLookup)
-	if err != nil {
-		t.FailNow()
-	}
+	doa.Doa(bytes.Contains(out, []byte("zhihu")))
 }
 
 func TestResolverAll(t *testing.T) {
@@ -103,9 +71,7 @@ func TestResolverAll(t *testing.T) {
 		ResolverPublic.Tencent.Doh,
 	} {
 		dns := ResolverAny(url)
-		_, err := dns.LookupHost(context.Background(), HostLookup)
-		if err != nil {
-			t.FailNow()
-		}
+		err := doa.Err(dns.LookupHost(context.Background(), HostLookup))
+		doa.Nil(err)
 	}
 }
